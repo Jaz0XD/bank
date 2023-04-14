50b806ae03663eb96d35ecb1755e22e89e6e4521
JazXD <110969648+Jaz0XD@users.noreply.github.com>
2023-04-14 18:38:21 +0530

    First upload

---
+++ js/app.js
@@ -0,0 +1,5 @@
+document.querySelector("#log").addEventListener("click", function(){
+    document.querySelector("#head").style="display: none";
+    //document.querySelector("#signup").style="display: none";
+    document.querySelector("#login").style="display: visible";
+})
